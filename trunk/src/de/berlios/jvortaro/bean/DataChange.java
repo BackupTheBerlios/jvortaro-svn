e3d803413d5477c334ef2d21d6102b0d0d37bc1f
efracasso <efracasso@18e1356f-6502-0410-a22a-f5a59181729e>
2005-11-01 11:03:27 +0000

    added DataChange bean

---
+++ trunk/src/de/berlios/jvortaro/bean/DataChange.java
@@ -0,0 +1,43 @@
+/*
+ * DictChange.java
+ *
+ * Created on 31 ottobre 2005, 20.57
+ *
+ * Copyright (C) 2005  Enrico Fracasso <efracasso@users.berlios.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package de.berlios.jvortaro.bean;
+
+/**
+ *
+ * @author enrico
+ */
+public class DataChange {
+    
+    public enum State {DELETE, INSERT};
+    private State state;
+    
+    /********** state *********/
+    public void setState (State state){
+        this.state = state;
+    }
+    
+    public State getState(){
+        return state;
+    }
+}
